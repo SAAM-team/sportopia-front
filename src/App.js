8a8c3be21dc868ce18489574a7bc9fc5393d1863
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-12-12 14:41:14 +0200

    fixing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import Header from './components/header/header'
-import Products from './components/products/products'
-import Carousel from './components/carousel/carousel'
+import Header from './components/header/header';
+import Products from './components/products/products';
 import { EachCategory } from './components/circle/circle';
-import { BrowserRouter, Route, Switch,Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import CategoryProds from './components/category-prods/category-prods';
 import BiddingMain from './components/bidding/bidding-main';
 import BiddingRoom from './components/bidding/bidding-room';
 import GlobalState from './context/global-state';
-import Header from './components/header/header';
-import Products from './components/products/products';
 import SignIn from './components/signin';
 import Admin from './components/admin/index';
 // Component
@@ -16,21 +13,21 @@ import Admin from './components/admin/index';
 function App() {
   return (
     <>
-<BrowserRouter>
-      <GlobalState>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Products} />
-          <Route exact path='/register' component={SignIn} />
-          <Route exact path='/bidding' component={BiddingMain} />
-          <Route path='/bidding/:id' component={BiddingRoom} />
-          <Route path='/admin' component={Admin} />
-          <Route path='/circle'>
-            <EachCategory />
-          </Route>
-          <Route path='/:id' component={CategoryProds} />
-        </Switch>
-      </GlobalState>
+      <BrowserRouter>
+        <GlobalState>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={Products} />
+            <Route exact path='/register' component={SignIn} />
+            <Route exact path='/bidding' component={BiddingMain} />
+            <Route path='/bidding/:id' component={BiddingRoom} />
+            <Route path='/admin' component={Admin} />
+            <Route path='/circle'>
+              <EachCategory />
+            </Route>
+            <Route path='/:id' component={CategoryProds} />
+          </Switch>
+        </GlobalState>
       </BrowserRouter>
     </>
   );
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
-import { getRemoteData } from '../../reducers/categories-action';
+import {
+  getRemoteData,
+  activeCategory
+} from '../../reducers/categories-action';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import {
@@ -34,26 +37,23 @@ import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import PropTypes from 'prop-types';
 import ShoppingCartRoundedIcon from '@material-ui/icons/ShoppingCartRounded';
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
-import a1 from "./assets/11.gif";
-import a2 from "./assets/12.gif";
-import a3 from "./assets/13.gif";
-import a4 from "./assets/14.gif";
-import a5 from "./assets/15.gif";
-import a6 from "./assets/16.gif";
-import a7 from "./assets/17.gif";
-import a8 from "./assets/18.gif";
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import a1 from './assets/11.gif';
+import a2 from './assets/12.gif';
+import a3 from './assets/13.gif';
+import a4 from './assets/14.gif';
+import a5 from './assets/15.gif';
+import a6 from './assets/16.gif';
+import a7 from './assets/17.gif';
+import a8 from './assets/18.gif';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import { Link } from 'react-router-dom';
-
-const avatarIcons = [a1,  a2,  a3,  a4,  a5,  a6,  a7,  a8]
-
 import avatar from './avatar.gif';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
 import Auth from '../../auth/auth';
+
+const avatarIcons = [a1, a2, a3, a4, a5, a6, a7, a8];
 
 function ScrollTop(props) {
   const { children, window } = props;
@@ -110,28 +110,29 @@ function Header(props) {
   const list = (anchor) => (
     <div
       // style={{backgroundColor: '#6BAB90', height: '100%', color:'E1F0C4' }}
-        className={classes.list}
-        role="presentation"
-        onClick={()=>props.getRemoteData()}
-        onClick={toggleDrawer(anchor, false)}
-      >
-          <Typography style={{padding:'10px 0px 5px 10px' }} variant="h4" noWrap>
-              Categories
-            </Typography>
-            <Divider />
-        <List>
-          {
-          props.categories.map((category, index) =>
-              (
-                <>
+      className={classes.list}
+      role='presentation'
+      onClick={() => props.getRemoteData()}
+      onClick={toggleDrawer(anchor, false)}
+    >
+      <Typography style={{ padding: '10px 0px 5px 10px' }} variant='h4' noWrap>
+        Categories
+      </Typography>
+      <Divider />
+      <List>
+        {props.categories.map((category, index) => (
+          <>
             <ListItem button key={category.id}>
               <ListItemAvatar>
-          <Avatar alt="" src={avatarIcons[index]} />
-        </ListItemAvatar>
-              <ListItemText primary={category.category_name} onClick={() => {
-                props.activeCategory(category.id)
-                // props.getRemoteData()
-                }}/>
+                <Avatar alt='' src={avatarIcons[index]} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={category.category_name}
+                onClick={() => {
+                  props.activeCategory(category.id);
+                  // props.getRemoteData()
+                }}
+              />
             </ListItem>
             <Divider />
           </>
@@ -443,10 +444,9 @@ const useStyles = makeStyles((theme) => ({
 
 const mapStateToProps = (state) => {
   console.log('this state for header component', state);
-    return {
-      categories: state.categories.results,
-
-    };
+  return {
+    categories: state.categories.results
   };
-  const mapDispatchToProps = { getRemoteData, activeCategory };
-  export default connect(mapStateToProps, mapDispatchToProps)(Header);
+};
+const mapDispatchToProps = { getRemoteData, activeCategory };
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
